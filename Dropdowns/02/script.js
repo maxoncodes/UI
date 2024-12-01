eebc5a978edc7706ee853de76427a61af93b4ab0
MAXON <maxonofficial1@gmail.com>
2024-12-01 21:25:52 +0530

    Delete Dropdowns directory

--- Dropdowns/02/script.js
+++
@@ -1,8 +0,0 @@
-const dropdownToggle = document.querySelector(".dropdown-toggle");
-
-dropdownToggle.addEventListener("click", () => {
-  const dropdownMenu = document.querySelector("#dropdown > .menu");
-
-  dropdownMenu.classList.toggle("open");
-  dropdownToggle.classList.toggle("open");
-});
